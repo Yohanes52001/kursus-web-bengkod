44bd4c0fa53b044cafe00b35084fa902bac2f590
Yohanes52001 <111202012551@mhs.dinus.ac.id>
2023-10-10 23:16:39 +0700

    buat file konsep

---
+++ PHP_Kondisional.php
@@ -0,0 +1,28 @@
+<?php
+    $angka = -5;
+    if($angka < 0){
+        echo 'Angka Negatif';
+    } else {
+        echo 'Angka Positif';
+    };
+    // Output : Angka Negatif
+
+    $input = 3;
+    switch($input){
+        case "1":
+        echo "Inputan adalah satu";
+        break;
+        case "2":
+        echo "Inputan adalah dua";
+        break;
+        case "3":
+        echo "Inputan adalah tiga";
+        break;
+        default:
+        echo "Tidak ada input yang cocok dengan pilihan anda";
+    }
+    /* Output : Inputan adalah tiga
+    Jika nilai dari variabel '$input' bukan sama dengan 'case',
+    maka akan menampilkan pesan "Tidak ada input yang cocok"
+    */
+?>
