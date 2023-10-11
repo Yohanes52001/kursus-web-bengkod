bb7a2e039e0042a066254602245e2117fb97e7fd
Yohanes52001 <111202012551@mhs.dinus.ac.id>
2023-10-11 19:09:34 +0700

    buat file konsep

---
+++ PHP_Empty.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = "";
+    if(empty($nama)) {
+        print("Nama kosong");
+    } else {
+        print("Nama tidak kosong");
+    }
+    // Output : Nama kosong    
+?>
---
+++ PHP_Isset.php
@@ -0,0 +1,9 @@
+<?php
+ $nama = "Bengkel Koding";
+ if(isset($nama)) {
+    print("Nama telah diatur");
+ } else {
+    print("Nama belum diatur");
+ }
+ // Output :Nama telah diatur
+?>
