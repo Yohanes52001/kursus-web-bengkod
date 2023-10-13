6009d8dbd0c32f47317fdece2db8dfa979ed9d36
Yohanes52001 <111202012551@mhs.dinus.ac.id>
2023-10-13 10:59:15 +0700

    buat file konsep

---
+++ PHP_LoopingForeach.php
@@ -0,0 +1,12 @@
+<?php
+$buah = array("Apel", "Jeruk", "Mangga", "Pisang");
+foreach ($buah as $item) {
+    echo $item . "\n";
+}
+/* Output : 
+Apel
+Jeruk
+Mangga
+Pisang
+*/
+?>
---
+++ PHP_LoopingWhile.php
@@ -0,0 +1,8 @@
+<?php
+    $x = 0; //Awal variabel $x
+    while($x <= 5) {
+        echo "$x";
+        $x++;
+    }
+    // Output : 0 1 2 3 4 5
+?>
